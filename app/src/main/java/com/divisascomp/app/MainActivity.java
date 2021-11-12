fab1e3855ba51e722645266cd49892d31d86afe2
Julian Sarmiento <juliansarmiento10@outlook.com>
2021-11-11 23:25:32 -0500

    SpinnerEvent

--- app/src/main/java/com/divisascomp/app/MainActivity.java
+++ app/src/main/java/com/divisascomp/app/MainActivity.java
@@ -4,22 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner listDiv, listDiv2;
+    TextView txtFrom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listDiv =  (Spinner) findViewById(R.id.lisDiv);
         listDiv2 =  (Spinner) findViewById(R.id.lisDiv2);
-
+        txtFrom = (TextView) findViewById(R.id.txtFrom);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.divisas_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         listDiv.setAdapter(adapter);
         listDiv2.setAdapter(adapter);
+        listDiv.setOnItemSelectedListener(this);
     }
 
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String from = parent.getItemAtPosition(position).toString();
+        txtFrom.setText(from);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
