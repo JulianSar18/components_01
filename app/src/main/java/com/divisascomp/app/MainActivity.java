a222f8c7eea9f3b80a98a410aaa081ea4bf999ce
Julian Sarmiento <juliansarmiento10@outlook.com>
2021-11-13 16:31:09 -0500

    divisas Listas

--- app/src/main/java/com/divisascomp/app/MainActivity.java
+++ app/src/main/java/com/divisascomp/app/MainActivity.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner listDiv, listDiv2;
-    TextView txtFrom;
+    TextView txtFrom, txtTo;
+    EditText edt1From;
+    double mon1;
+    double cambio;
+    String cambioc;
+    String select, select2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         listDiv =  (Spinner) findViewById(R.id.lisDiv);
         listDiv2 =  (Spinner) findViewById(R.id.lisDiv2);
         txtFrom = (TextView) findViewById(R.id.txtFrom);
+        txtTo = (TextView) findViewById(R.id.txtTo);
+        edt1From = (EditText) findViewById(R.id.edtFrom);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.divisas_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         listDiv.setAdapter(adapter);
@@ -30,15 +35,167 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         listDiv.setOnItemSelectedListener(this);
     }
 
-
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String from = parent.getItemAtPosition(position).toString();
         txtFrom.setText(from);
     }
 
+
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+    }
 
+
+    public void convertBtn(View view){
+        ctuUtc();
+        ctaAtc();
+        ctyYtc();
+        ctwWtc();
+        utaAtu();
+        utjJtu();
+        utwWtu();
+        atjJta();
+        atwWta();
+        jtwWtj();
+    }
+    public void ctuUtc (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("COP $")&&select2.equals("USD $")){
+            cambio= mon1 / 3900;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("USD $")&&select2.equals("COP $")){
+            cambio= mon1 * 3900;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
+    }
+    public void ctaAtc (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("COP $")&&select2.equals("AUD A$")){
+            cambio= mon1 / 2847.08;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("AUD A$")&&select2.equals("COP $")){
+            cambio= mon1 * 2847.08;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
+    }
+    public void ctyYtc (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("COP $")&&select2.equals("JPY ¥")){
+            cambio= mon1 / 34.09;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("JPY ¥")&&select2.equals("COP $")){
+            cambio= mon1 * 34.09;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
+    }
+    public void ctwWtc(){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("COP $")&&select2.equals("KRW ₩")){
+            cambio = mon1 / 3;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("KRW ₩")&&select2.equals("COP $")){
+            cambio = mon1 * 3;
+            txtTo.setText(cambioc);
+        }
+    }
+    public void utaAtu (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("USD $")&&select2.equals("AUD A$")){
+            cambio= mon1 / 0.73;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("AUD A$")&&select2.equals("USD $")){
+            cambio= mon1 * 0.73;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
+    }
+    public void utjJtu (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("USD $")&&select2.equals("JPY ¥")){
+            cambio= mon1 / 0.0088;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("JPY ¥")&&select2.equals("USD $")){
+            cambio= mon1 * 0.0088;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
+    }
+    public void utwWtu (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("USD $")&&select2.equals("KRW ₩")){
+            cambio= mon1 / 0.00085;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("KRW ₩")&&select2.equals("USD $")){
+            cambio= mon1 * 0.00085;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
+    }
+    public void atjJta (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("AUD A$")&&select2.equals("JPY ¥")){
+            cambio= mon1 / 0.012;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("JPY ¥")&&select2.equals("AUD A$")){
+            cambio= mon1 * 0.012;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
+    }
+    public void atwWta (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("AUD A$")&&select2.equals("KRW ₩")){
+            cambio= mon1 / 0.0012;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("KRW ₩")&&select2.equals("AUD A$")){
+            cambio= mon1 * 0.0012;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
+    }
+    public void jtwWtj (){
+        mon1 = Integer.parseInt(edt1From.getText().toString());
+        select = listDiv.getSelectedItem().toString();
+        select2 = listDiv2.getSelectedItem().toString();
+        if(select.equals("JPY ¥")&&select2.equals("KRW ₩")){
+            cambio= mon1 / 0.097;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }else if (select.equals("KRW ₩")&&select2.equals("JPY ¥")){
+            cambio= mon1 * 0.097;
+            cambioc = String.valueOf(cambio);
+            txtTo.setText(cambioc);
+        }
     }
 }
